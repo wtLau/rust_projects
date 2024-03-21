186f698cd82aa83cfe0af68b79fac9cac85d1a94
Brian <26396307+wtLau@users.noreply.github.com>
2024-03-21 15:31:53 -0700

    successully spin up a web server

---
+++ actix-gcd/src/main.rs
@@ -0,0 +1,27 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+#[actix_web::main]
+async fn main() {
+    let app = || App::new().route("/", web::get().to(get_index));
+
+    let server = HttpServer::new(app)
+        .bind("127.0.0.1:8080")
+        .expect("Error bindign server to address");
+
+    println!("Serving on http://localhost:8080....");
+
+    server.run().await.expect("error running server");
+}
+
+async fn get_index() -> HttpResponse {
+    HttpResponse::Ok().content_type("text/html").body(
+        r#"
+            <title> GCD Calculator</title>
+            <form action="/gcd" method="post">
+                <input type="text" name="n"/>
+                <input type="text" name="m"/>
+                <button type="submit"> Compute GCD</button>
+            </form>
+            "#,
+    )
+}
