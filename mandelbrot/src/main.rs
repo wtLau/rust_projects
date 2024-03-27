3e1c3a224f6c8c2b3cb8970d8c2f882f4922b3b2
Brian <26396307+wtLau@users.noreply.github.com>
2024-03-26 23:11:38 -0700

    cocurrent rendering- from 3.72s to 0.1s

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -21,16 +21,32 @@ fn main() {
     // creating a rectangle array inital to 0 and length of bounds
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    render(&mut pixels, bounds, upper_left, lower_right);
+    // Multi Thread Method - Cocurrent
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right =
+                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+
+                spawner.spawn(move |_| {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        })
+        .unwrap();
+    }
+    // Single Thread Method
+    // render(&mut pixels, bounds, upper_left, lower_right);
 
     write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
-}
-
-fn complex_square_add_loop(c: Complex<f64>) {
-    let mut z = Complex { re: 0.0, im: 0.0 };
-    loop {
-        z = z * z + c;
-    }
 }
 
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
