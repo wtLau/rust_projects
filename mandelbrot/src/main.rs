e06c1ca1a4fd97fe6f213461ad5ec9798a672ec6
Brian <26396307+wtLau@users.noreply.github.com>
2024-03-26 22:27:08 -0700

    writing the main mandelbrot cocurrent  program

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,7 +1,30 @@
 use image::{png::PNGEncoder, ColorType};
 use num::Complex;
+use std::env;
 
-fn main() {}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!(
+            "Example: {} madel.png 1000x750 -1.20, 0.35, -1, 0.20",
+            args[0]
+        );
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("Error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+
+    // creating a rectangle array inital to 0 and length of bounds
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
+}
 
 fn complex_square_add_loop(c: Complex<f64>) {
     let mut z = Complex { re: 0.0, im: 0.0 };
