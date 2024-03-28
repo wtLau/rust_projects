b01c8f924011f4ae11edad3ea57a9aed589ce6b1
Brian <26396307+wtLau@users.noreply.github.com>
2024-03-27 23:22:56 -0700

    arg correctly piced up and placed in the Arg struct

---
+++ quickreplace/src/main.rs
@@ -0,0 +1,47 @@
+#[derive(Debug)]
+struct Arguments {
+    target: String,
+    replacement: String,
+    filename: String,
+    output: String,
+}
+
+fn main() {
+    let args = parse_args();
+    println!("{:?}", args);
+}
+
+use text_colorizer::*;
+
+fn print_usage() {
+    eprintln!(
+        "{} - change occurrences of one string into another",
+        "quickreplace".green()
+    );
+    eprintln!("Usage: quickreplace <targe> <replacement> <INPUT> <OUTPUT>");
+}
+
+use std::env;
+
+fn parse_args() -> Arguments {
+    // skipping the name of the program being run
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() != 4 {
+        print_usage();
+        eprintln!(
+            "{} wrong number of arguments: expect 4, got {}.",
+            "Error:".red().bold(),
+            args.len()
+        );
+
+        std::process::exit(1);
+    }
+
+    Arguments {
+        target: args[0].clone(),
+        replacement: args[1].clone(),
+        filename: args[2].clone(),
+        output: args[3].clone(),
+    }
+}
