68a6f03f845ef8b23f8bc9cade5fc2c502c73068
Brian <26396307+wtLau@users.noreply.github.com>
2024-04-11 11:59:14 -0700

    change comment so it can appear on rust doc

--- turn-program-into_library/src/lib.rs
+++ turn-program-into_library/src/lib.rs
@@ -4,13 +4,13 @@ pub struct Fern {
 }
 
 impl Fern {
-    // Simulate a fern growing for one day
+    /// Simulate a fern growing for one day
     pub fn grow(&mut self) {
         self.size *= 1.0 + self.growth_rate;
     }
 }
 
-// Run a fern simulation for some number of days
+/// Run a fern simulation for some number of days
 pub fn run_simulation(fern: &mut Fern, days: usize) {
     for _ in 0..days {
         fern.grow();
