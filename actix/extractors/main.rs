1820b700e715a93d1fcbe9177fbfce56fd82dec0
Brian <26396307+wtLau@users.noreply.github.com>
2024-06-17 12:32:19 -0700

    learned extractors

---
+++ actix/extractors/main.rs
@@ -0,0 +1,18 @@
+use actix_web::{get, web, App, HttpServer, Result};
+
+/// extract path info from "/users/{user_id}/{friend}" url
+/// {user_id} - deserializes to a u32
+/// {friend} - deserializes to a String
+#[get("/users/{user_id}/{friend}")] // <- define path parameters
+async fn index(path: web::Path<(u32, String)>) -> Result<String> {
+    let (user_id, friend) = path.into_inner();
+    Ok(format!("Welcome {}, user_id {}!", friend, user_id))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
