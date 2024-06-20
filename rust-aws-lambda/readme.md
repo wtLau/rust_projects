d23d6e861fad922420f54d4e83ca610c6739f817
Brian <26396307+wtLau@users.noreply.github.com>
2024-06-20 11:29:51 -0700

    deployed to AWs lambda via cli

---
+++ rust-aws-lambda/readme.md
@@ -0,0 +1,3 @@
+First AWS Lambda deployed!
+
+https://segujxgmwkgr7enunswpbzdn6m0xyjvg.lambda-url.us-west-2.on.aws/
--- rust-aws-lambda/src/main.rs
+++ rust-aws-lambda/src/main.rs
@@ -1,30 +1,16 @@
-use lambda_http::{run, service_fn, tracing, Body, Error, Request, RequestExt, Response};
-
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
-
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/html")
-        .body(message.into())
-        .map_err(Box::new)?;
-    Ok(resp)
-}
+use lambda_runtime::{service_fn, Error, LambdaEvent};
+use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    tracing::init_default_subscriber();
+    let func = service_fn(func);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
 
-    run(service_fn(function_handler)).await
+async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
+    let (event, _context) = event.into_parts();
+    let first_name = event["firstName"].as_str().unwrap_or("world");
+
+    Ok(json!({ "message": format!("Hello, {}!", first_name) }))
 }
